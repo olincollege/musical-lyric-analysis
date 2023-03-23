7370c748a5284a80fe31488ed6717b65f87fca6b
Carter <github@ke8oho.com>
2023-03-22 22:35:11 -0400

    Add ability to write lyrics to file
    Update genius_lyrics.py to add functions to write to and load lyrics
    from a file automatically

--- genius_lyrics.py
+++ genius_lyrics.py
@@ -1,5 +1,6 @@
 import lyricsgenius as lg
 import api_keys as key
+import csv
 
 LIST_OF_ALBUM_KEYWORDS = ["Broadway", "Cast", "Recording"]
 
@@ -11,7 +12,7 @@ PUNCTUATION_MARKS = "\"!#$%&'()*+,-./:;<=>?@\^_`{|}~"
 
 # List of random sequences that make it into lyrics that, if found, should be
 # removed
-REMOVE_FROM_LYRICS = ["\u200b", "Translations", "Embed"]
+REMOVE_FROM_LYRICS = ["\u200b", "translations", "embed"]
 
 
 def find_album(name):
@@ -78,7 +79,7 @@ def find_album(name):
     # current_album_tracks = genius_object.album_tracks(album["result"]["id"])
 
 
-def get_all_lyrics(album_id):
+def download_all_lyrics(album_id):
     """
     Given an ID of a genius album, get all lyrics of all songs on that album.
 
@@ -129,16 +130,10 @@ def get_all_lyrics(album_id):
         # lyricsgenius library scraped
         song_lyrics = genius_object.lyrics(song_id)
 
-        # This line replaces all punctuation marks in the string with empty 
+        # This line replaces all punctuation marks in the string with empty
         # space so that words are not marked as unique just because they have
         # punctuation marks in them.
         song_lyrics = song_lyrics.translate(str.maketrans("", "", PUNCTUATION_MARKS))
-        
-        # Upon testing, several weird sequences were found within the lyrics 
-        # that must be removed to prevent skewing the results. Each is replaced
-        # with empty space.
-        for word in REMOVE_FROM_LYRICS:
-            song_lyrics.replace(word, '')
 
         # The single string containing all lyrics in the song is split into a
         # list. Without another parameter, the split function will by default
@@ -153,19 +148,86 @@ def get_all_lyrics(album_id):
         song_lyrics_filtered = [
             word.lower()
             for word in song_lyrics_split
-            if (
-                ("[" not in word)
-                and ("]" not in word)
-            )
+            if (("[" not in word) and ("]" not in word))
         ]
+
+        # The results returned by the lyricsgenius library were found to
+        # consistently contain extra garbage with the first and second word.
+        # To alleviate this, the only way to reliably handle this is to
+        # remove these words completely.
+        song_lyrics_filtered = song_lyrics_filtered[1 : len(song_lyrics_filtered) - 1]
 
         # Each list is appended to the master list for all songs in the album.
         album_lyrics.append(song_lyrics_filtered)
 
     return album_lyrics
 
+
+def write_lyrics_to_file(album_id):
+    """
+    Save a musical's lyrics to a CSV file.
+
+    Since it takes an incredibly long time to search Genius for the lyrics of
+    an entire musical, it makes the most sense to complete this process once
+    and then save it to a CSV file so it only has to be ran once.
+
+    The CSV file is formatted such that each row represents a song and a one
+    word is placed in each column. The album's Genius ID will be used as the
+    filename for ease of use.
+
+    Args:
+        album_id: string, numerical ID for an album on Genius.
+    Returns:
+        Nothing.
+    """
+
+    lyrics = download_all_lyrics(album_id)
+
+    filepath = f"lyrics/{album_id}.csv"
+
+    with open(filepath, "w") as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(lyrics)
+
+
+def get_all_lyrics(album_id):
+    """
+    Given an album ID, this function will first try to load the lyrics from a
+    file if they are already downloaded. If the album has not already been
+    downloaded, it is grabbed from Genius and then loaded from the file
+    created.
+
+    Args:
+        album_id: string representing the album's numerical Genius ID
+    Returns:
+        List of lists, which each embedded list containing strings for each
+            individual word in a songs lyrics. Each song on the album
+            gets its own embedded list.
+    """
+
+    file_path = f"lyrics/{album_id}.csv"
+
+    lyrics = []
+    try:
+        with open(file_path, 'r', encoding="utf-8") as file:
+            # Use CSV library to open CSV; create list of lists in the format
+            # that we are looking for.
+            csv_reader = csv.reader(file)
+            lyrics = list(csv_reader)
+    except FileNotFoundError:
+        write_lyrics_to_file(album_id)
+        with open(file_path, 'r', encoding="utf-8") as file:
+            # Use CSV library to open CSV; create list of lists in the format
+            # that we are looking for.
+            csv_reader = csv.reader(file)
+            lyrics = list(csv_reader)
+
+    
+    return(lyrics)
+
+
 def calculate_lyrical_uniqueness(lyrics):
-    """"
+    """ "
     Calculates the lyrical uniqueness of a song. Lyrical uniqueness is defined
     as the percentage of unique words compared to the total number of words in a
     song.
@@ -174,7 +236,7 @@ def calculate_lyrical_uniqueness(lyrics):
         lyrics: list of strings representing all of the individual words in a
             song.
     Returns:
-        Integer (rounded) percentage of words that are unique in a song 
+        Integer (rounded) percentage of words that are unique in a song
     """
 
     unique_words = []
@@ -182,8 +244,9 @@ def calculate_lyrical_uniqueness(lyrics):
     for word in lyrics:
         if word not in unique_words:
             unique_words.append(word)
-    
+
     return int((len(unique_words) / len(lyrics)) * 100)
+
 
 def calculate_album_uniqueness(all_album_lyrics):
     """
@@ -195,8 +258,8 @@ def calculate_album_uniqueness(all_album_lyrics):
 
     Args:
         all_album_lyrics: list of lists of strings, which each embedded list
-            containing each word in the lyrics of one of the album's song as 
-            individual strings.  
+            containing each word in the lyrics of one of the album's song as
+            individual strings.
     Return:
         Integer representing the percent uniqueness of an album's lyrics, on
             average
@@ -206,5 +269,5 @@ def calculate_album_uniqueness(all_album_lyrics):
 
     for song in all_album_lyrics:
         total_percentages += calculate_lyrical_uniqueness(song)
-    
+
     return int(total_percentages / len(all_album_lyrics))
