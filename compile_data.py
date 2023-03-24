3d964a3b8abf04c4264c96e7cfdce52e0481bb29
Elin O'Neill <eoneill@olin.edu>
2023-03-24 14:37:44 -0400

    created function in compile_data.py

--- compile_data.py
+++ compile_data.py
@@ -1,6 +1,8 @@
 import genius_lyrics as lyrics
+import pandas as pd
 
-def find_corresponding_album(broadway_data_path="summed_broadway_data.csv"):
+
+def find_corresponding_album():
     """
     Find the lyrical uniqueness of all songs in the Broadway data set.
 
@@ -9,3 +11,23 @@ def find_corresponding_album(broadway_data_path="summed_broadway_data.csv"):
     are added to the dataframe. If a match is not able to be identified for a
     musical, then it is removed from the dataset.
     """
+    list_musical_title = []
+    list_musical_genius_id = []
+    list_album_title = []
+
+    with open("summed_broadway_data.csv", "r", encoding="utf-8") as file:
+        musical_data = pd.read_csv(file)
+
+    list_musical_title.extend(musical_data["ShowName"].tolist())
+
+    for musical_title in list_musical_title:
+        (musical_genius_id, album_title) = lyrics.find_album(musical_title)
+        list_musical_genius_id.append(musical_genius_id)
+        list_album_title.append(album_title)
+
+    musical_data["GeniusID"] = list_musical_genius_id
+    musical_data["AlbumTitle"] = list_album_title
+    musical_data = musical_data[musical_data["GeniusID"] != "-1"]
+    musical_data = musical_data.reset_index(drop=True)
+
+    musical_data.to_csv("musical_genius_data.csv", encoding="utf-8", index=False)
